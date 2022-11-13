0c3263fc66b08732169be9fd399367396521a1b3
Raef <bakleh.raef@gmail.com>
2022-11-14 00:29:53 +0100

    Mobile Layout

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bbe13024eb3bd389"; }
+/******/ 		__webpack_require__.h = function() { return "7bfd55b4e5f73fbf"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
