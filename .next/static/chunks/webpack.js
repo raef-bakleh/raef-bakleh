b56d2d52f0c33af8804c51efb3fdf3d2183eb177
Raef <bakleh.raef@gmail.com>
2022-11-20 01:00:23 +0100

    input text color

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "72c867b6095ab879"; }
+/******/ 		__webpack_require__.h = function() { return "1985532f883fea83"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
