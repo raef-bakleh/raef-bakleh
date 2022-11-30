88ed73d13e89e137659f8acc6d62255c881f9387
Raef <bakleh.raef@gmail.com>
2022-11-30 18:29:48 +0100

    Sidebar Closing animation

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "64de594ea95722c0"; }
+/******/ 		__webpack_require__.h = function() { return "7e9ed4233fa72041"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
