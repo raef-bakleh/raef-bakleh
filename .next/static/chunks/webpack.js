eb9efb3cf5e2d538bab3d9e7d4c516b6da0832a8
Raef <bakleh.raef@gmail.com>
2022-12-13 00:52:59 +0100

    marquee changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a2650baf91302d57"; }
+/******/ 		__webpack_require__.h = function() { return "75a4292bcfa89ca8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
